1ff42dc84d86d5ad314982acb463d0ceb00d53c8
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-10-31 12:45:41 +0200

    use `use`

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Routes configuration
 app.use('/api', routes)
 app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html')))
+app.use((req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html')))
 
 
 // Express configuration
