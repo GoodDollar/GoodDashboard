8d2d52b494f7a1bcb34ec9e080500f631902e2f3
Hadar Rottenberg <hadar@gooddollar.org>
2019-12-06 00:31:18 +0200

    add: unique_txs as set, so addition will be correct

--- server/src/models/about-transaction.ts
+++ server/src/models/about-transaction.ts
@@ -1,14 +1,14 @@
-import mongoose from '../mongo-db.js'
-import { MODEL_ABOUT_TRANSACTION } from './constants'
+import mongoose from "../mongo-db.js";
+import { MODEL_ABOUT_TRANSACTION } from "./constants";
 
 export const aboutTransactionSchema = new mongoose.Schema({
   count_txs: Number,
   amount_txs: Number,
-  unique_txs: Number,
+  unique_txs: [String],
   date: {
     type: String,
     index: { unique: true }
-  },
-})
+  }
+});
 
-export default mongoose.model(MODEL_ABOUT_TRANSACTION, aboutTransactionSchema)
+export default mongoose.model(MODEL_ABOUT_TRANSACTION, aboutTransactionSchema);
