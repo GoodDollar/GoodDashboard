d79359aa9def4934c76d2b8463d1535dfb73970a
StanislavShevchenko <stas246978@gmail.com>
2019-12-18 11:06:03 +0200

    fixed LGTM

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,8 +14,6 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Routes configuration
 app.use('/api', routes)
 app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
-app.use((req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html')))
-
 
 // Express configuration
 app.set('port', process.env.PORT || 3055)
