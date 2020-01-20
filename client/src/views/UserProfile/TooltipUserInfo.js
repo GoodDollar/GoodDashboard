94138eabdbe946812e95aa3938e6b242e5ffc3a8
StanislavShevchenko <stas246978@gmail.com>
2020-01-20 17:06:56 +0200

    fixed LGTM

--- client/src/views/UserProfile/TooltipUserInfo.js
+++ client/src/views/UserProfile/TooltipUserInfo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { withStyles, makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Tooltip from '@material-ui/core/Tooltip'
