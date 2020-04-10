8d3e1897f536f352096bcc744c007687679eddc9
Hadar Rottenberg <hadar@gooddollar.org>
2020-04-10 10:06:19 +0300

    fix: react env names

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 export default {
   env: env.REACT_APP_ENV || "development",
-  walletUrl: env.REACT_WALLET_URL || "https://ewallet.gooddollar.org",
-  gunUrl: env.REACT_GUN_URL || "https://etorogun-prod.herokuapp.com/gun",
+  walletUrl: env.REACT_APP_WALLET_URL || "https://ewallet.gooddollar.org",
+  gunUrl: env.REACT_APP_GUN_URL || "https://etorogun-prod.herokuapp.com/gun",
   apiUrl: env.REACT_APP_API_URL,
 };
