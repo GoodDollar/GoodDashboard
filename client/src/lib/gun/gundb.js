7740028b928075ebc56c546171aef02edd59c43d
Hadar Rottenberg <hadar@gooddollar.org>
2020-04-21 17:03:55 +0300

    fix: gun then

--- client/src/lib/gun/gundb.js
+++ client/src/lib/gun/gundb.js
@@ -1,11 +1,12 @@
 import Gun from "gun";
+import "gun/lib/then";
 import Config from "../../config";
 
 let gunDb;
 
 const initGunDB = () => {
   if (!gunDb) {
-    gunDb = Gun({
+    global.gun = gunDb = Gun({
       localStorage: true,
       peers: [Config.gunUrl],
     });
