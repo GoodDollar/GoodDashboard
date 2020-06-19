fcb5ae67010370805d6416cf9592361c35dfdd98
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-06-19 16:03:50 +0300

    add: add default values for new route

--- server/src/controllers/gd.ts
+++ server/src/controllers/gd.ts
@@ -40,9 +40,9 @@ const getInEscrow = async (req: Request, res: Response, next: NextFunction) => {
 
 const totalImpactStatistic = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const totalUniqueClaimers = await propertyProvider.get('totalUniqueClaimers')
-    const totalUBIDistributed = await propertyProvider.get('totalUBIDistributed')
-    const totalGDVolume = await propertyProvider.get('totalGDVolume')
+    const totalUniqueClaimers = (await propertyProvider.get('totalUniqueClaimers')) || 0
+    const totalUBIDistributed = (await propertyProvider.get('totalUBIDistributed')) || 0
+    const totalGDVolume = (await propertyProvider.get('totalGDVolume')) || 0
 
     return res.status(200).json({
       responseCode: 200,
