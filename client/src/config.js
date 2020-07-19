04dd0fe398dac87141724968c9f524ee793496c7
Hadar Rottenberg <hadar@gooddollar.org>
2020-07-19 23:01:13 +0300

    fix: default dashboard api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
+
 const env = process.env;
-console.log({env})
 export default {
   env: env.REACT_APP_ENV || "development",
   walletUrl: env.REACT_APP_WALLET_URL,
   gunUrl: env.REACT_APP_GUN_URL,
-  apiUrl: env.REACT_APP_API_URL,
+  apiUrl: env.REACT_APP_API_URL || window && window.location && window.location.origin+"/api",
   embedDataStudioUrl: env.REACT_APP_EMBED_DATA_STUDIO_URL,
 };
