83e97fb57ec0b0e881495910a383294f8918bcdb
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2020-09-29 10:14:05 +0300

    fix: skip current day statistic for line graphs to prevent visual drops (#45)

--- server/src/helpers/reqLimit.ts
+++ server/src/helpers/reqLimit.ts
@@ -1,6 +1,12 @@
+const DEFAULT_QUERY_OFFSET = 1
+
 export default (req: any) => {
   let { limit } = req.query
-  let result = [0]
+
+  // The first element in array is offset - determines how much elements skip from query
+  // We are skipping the 1st record from query which is current day. The current day is still in progress so the data is not fully collected
+  // By doing this we prevent drops/gap in visual graphs for users
+  let result = [DEFAULT_QUERY_OFFSET]
 
   if (limit) {
     limit = parseInt(limit)
